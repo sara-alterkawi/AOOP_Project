8171be70ecb826787e0f395e9cd14f2acb1307bb
Sara Alterkawi <42577903+sara-alterkawi@users.noreply.github.com>
2023-05-10 00:37:05 +0200

    Update View.java

--- src/View.java
+++ src/View.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 public class View {
+  private final Model model;
+  private String dataSaver = null;
 }

c0afda070ca94453b95dce207941063b08c155f5
Sara Alterkawi <42577903+sara-alterkawi@users.noreply.github.com>
2023-05-10 00:37:38 +0200

    Update View.java

--- src/View.java
+++ src/View.java
@@ -6,4 +6,30 @@ import java.awt.event.KeyEvent;
 public class View {
   private final Model model;
   private String dataSaver = null;
+  
+  public View(Model model, String name) {
+        this.model = model;
+        model.addNotifyListener(this);
+        add(model);
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event){
+                if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    dataSaver = "RIGHT";
+                } else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+                    dataSaver = "LEFT";
+                } else if (event.getKeyCode() == KeyEvent.VK_UP) {
+                    dataSaver = "UP";
+                } else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+                    dataSaver = "DOWN";
+                }
+            }
+        });
+
+        setTitle(name); // Title of the View.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set the default close operation to EXIT_ON_CLOSE.
+        setSize(model.getWidth(),model.getHeight()); // Set the size of the View to the width and height of gameModel.
+        setVisible(true); // Sets the View to be visible.
+    }
 }

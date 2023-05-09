252ed1c75f388c01f2c089b5e75fdf9cc94f35a0
NashwaNanaa <132671312+NashwaNanaa@users.noreply.github.com>
2023-05-10 00:23:14 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        // Passing in the level data for level 1 obtained from the Levels.getLevelData().
+        Model gameModel = new Model(Levels.getCurrentLevel(1),1);
+        // "SOKOBAN" as the title of the game window.
+        View gameFrame = new View(gameModel, "SOKOBAN");
+        // Add a new KeyAdapter
+        gameFrame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event){
+                gameModel.gameInstruction(gameFrame);
+            }
+        });
+        ConsolePrinter consolePrint = new ConsolePrinter(gameModel);
+        Levels leve = new Levels(gameModel);
+    }
+
 }

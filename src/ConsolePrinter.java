9c823f985db132ed764297c9e7243465f7f776cd
Rahaf-Darouich <132815037+Rahaf-Darouich@users.noreply.github.com>
2023-05-10 00:16:22 +0200

    Update ConsolePrinter.java

--- src/ConsolePrinter.java
+++ src/ConsolePrinter.java
@@ -8,5 +8,17 @@ public ConsolePrinter(Model model) {
         this.model = model;
         model.addNotifyListener(this);
     }
+    /**
+     * This line is an annotation that indicates that the following method is meant
+     * to override a method from the parent class or interface.
+     * In this case, the method "stateChanged" from the "ChangeListener" interface is being overridden.
+     * The method takes a "ChangeEvent" object as a parameter.
+     * */
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        String output = generateOutput();
+        System.out.print(output);
+    }
+
 
 }

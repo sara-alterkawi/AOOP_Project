bc9f61405e1cc5210b321723802def150d661278
Rahaf-Darouich <132815037+Rahaf-Darouich@users.noreply.github.com>
2023-05-10 00:25:04 +0200

    Update Model.java

--- src/Model.java
+++ src/Model.java
@@ -10,6 +10,31 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Model extends JComponent {
-    
+    private String [][] gameBoard;
+    private int currentLevel;
+
+    private final LinkedList<ChangeListener> notifyListner;
+
+    private Point playerPosition;
+    private int numCols;
+    private int numRows;
+
+    private final static int boardWidth = 32;
+
+    private final static String wall = "w";
+    private final static String freeSpace = "f";
+    private final static String box = "x";
+    private final static String goalPlace = ".";
+    private final static String boxOnGoal = "*";
+    private final static String sokoban = "s";
+    private final static String sokobanOnGoal = "$";
+
+    private BufferedImage wallImg;
+    private BufferedImage freeSpaceImg;
+    private BufferedImage boxImg;
+    private BufferedImage goalPlaceImg;
+    private BufferedImage boxOnGoalImg;
+    private BufferedImage sokobanImg;
+
 
 }

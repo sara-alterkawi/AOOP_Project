3925c5bab3be59341bca0c59a4ce77446f86d8e5
Sara Alterkawi <42577903+sara-alterkawi@users.noreply.github.com>
2023-05-10 00:14:23 +0200

    Update ConsolePrinter.java

--- src/ConsolePrinter.java
+++ src/ConsolePrinter.java
@@ -1,2 +1,8 @@
-public class ConsolePrinter {
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class ConsolePrinter implements ChangeListener {
+    private final Model model;
+
+
 }

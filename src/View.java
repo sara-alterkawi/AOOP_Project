6d2d0581ae91c90628c03ea54d9f725b573833c2
Sara Alterkawi <42577903+sara-alterkawi@users.noreply.github.com>
2023-05-13 14:25:39 +0200

    KeyListener

--- src/View.java
+++ src/View.java
@@ -1,33 +1,26 @@
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class View extends JFrame implements ChangeListener, Controller {
+public class View extends JFrame implements ChangeListener, Controller, KeyListener {
 
     private final Model model;
     private String dataSaver = null;
 
     public View(Model model, String name) {
         this.model = model;
+        model.startTimer();
         model.addNotifyListener(this);
         add(model);
 
-        addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent event){
-                if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    dataSaver = "RIGHT";
-                } else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
-                    dataSaver = "LEFT";
-                } else if (event.getKeyCode() == KeyEvent.VK_UP) {
-                    dataSaver = "UP";
-                } else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-                    dataSaver = "DOWN";
-                }
-            }
-        });
+        JOptionPane.showMessageDialog(this, "How to play SOKOBAN!\n"
+                + "Move the box to the goal place using the arrow keys.\n"
+                + "You win when all the boxes are set on goal places.\n"
+                + "You can move only one box at a time.");
+
+        addKeyListener(this);
 
         setTitle(name); // Title of the View.
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set the default close operation to EXIT_ON_CLOSE.
@@ -47,5 +40,30 @@ public class View extends JFrame implements ChangeListener, Controller {
         setSize(model.getWidth(),model.getHeight()); // Set the size of the View to the width and height of gameModel
         model.repaint();
     }
+    
+     @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            dataSaver = "RIGHT";
+            Model.moved();
+        } else if (keyCode == KeyEvent.VK_LEFT) {
+            dataSaver = "LEFT";
+            Model.moved();
+        } else if (keyCode == KeyEvent.VK_UP) {
+            dataSaver = "UP";
+            Model.moved();
+        } else if (keyCode == KeyEvent.VK_DOWN) {
+            dataSaver = "DOWN";
+            Model.moved();
+        }
+        model.gameInstruction(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 
 }
